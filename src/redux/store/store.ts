08da4874ab2afeaf2f7d891eb041a37a7d59ecdd
123Somrat <jafaruddin875@gmail.com>
2024-06-27 13:43:27 +0200

    Create gloval types hooks for store and dispath

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -6,5 +6,7 @@ const store = configureStore({
       reducer:{}
 })
 
-
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
 export default store
